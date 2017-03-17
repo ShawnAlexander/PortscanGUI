0d84c93dc8ac720dc29bd962589010b90c6f7175
Shawn Johnson <shawn.a.johnson@ucdenver.edu>
2017-03-16 21:13:41 -0600

    3/16/17 9:13

--- view/listview.py
+++ view/listview.py
@@ -1,17 +1,27 @@
 from tkinter import ttk
 from tkinter import StringVar
 
+TESTDATA = [{'IP Version': '4', 'IP Address': '192.168.1.1', 'Port Range': '0-65000', 'Timeout': '5'}, {'IP Version': '4', 'IP Address': '192.168.0.1', 'Port Range': '0-65000', 'Timeout': '5'}]
+
 class ListView(ttk.Frame):
+    COLUMNS = ('ver', 'ip', 'port', 'time')
+    HEADINGS = ('IP Version', 'IP Address', 'Port Range', 'Timeout')
     def __init__(self, master, **kw):
-        ttk.Frame.__init__(self, master=master)
+        ttk.Frame.__init__(self, master=master, width=300)
         self.grid(row=0, column=0)
-        self.tBox = ttk.Treeview(master=self, height=30)
+        self.tBox = ttk.Treeview(master=self, height=30, columns=ListView.COLUMNS)
         self.scroll = ttk.Scrollbar(master=self, orient="vertical", command=self.tBox.yview)
         self.tBox.configure(yscrollcommand=self.scroll.set)
         self.scroll.grid(row = 0, column = 1)
         self.tBox.grid(row = 0, column = 0)
+        self.doHeadings()
+        self.addTestData()
 
-    def append(self, lofl):
-        for i in lofl:
-            j = StringVar(i)
-            self.tBox.insert(END, j)
+    def append(self, listOfDictionary):
+        for l in listOfDictionary:
+            self.tBox.insert('', index='end')
+    def doHeadings(self):
+        for col in ListView.COLUMNS:
+            
+    def addTestData(self):
+        self.append(TESTDATA)
